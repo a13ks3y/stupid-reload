6d49e513611d91da3ce353c86d795a1584f88194
a13ks3y.ass <a13ks3y.ass@gmail.com>
2018-12-21 08:09:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Stupid Reload
+This is just stupid.
+
+## Description
+Reloads the page every 0xfff milliseconds (4 seconds) by default, 
+if "live=true" string is presented in the hash url 
+`````
+http://example.com/index.html#live=true
+`````
+    
+Checks the __config.js__ file each 0xfff * 2 milliseconds
+(8 seconds) by default.
+
+## Installation & Usage
+### Warning! This section is not complete!
+@todo: update this section with correct urls and package name!
+
+To install cli tools and advanced stupid script use npm:
+````
+npm i stupid-reload --save-dev
+````
+
+Basically, if you don't need to change reload state 
+(switch off and then turn it on back) you can use just one line:
+````javascript
+setTimeout(location.reload.bind(location, [true], 0xfff));
+````
+Or use a [bookmarklet](https://en.wikipedia.org/wiki/Bookmarklet#Usage)
+just press create new bookmark in the browser of your choice, and in the 
+field "url" put this code:
+````html
+javascript:setTimeout(location.reload.bind(location, [true], 0xfff))
+````
+Or just drug [this link](javascript:setTimeout(p=>location.reload\(true\),0xfff))
+to your bookmarks bar.
+
+If it's not working, edit bookmark and make sure,
+that url is started with __"javascript:"__ (don't miss the semicolon!)
+at the very beginning.
+
+If you need to change reload state by dblclick somewhere on the page 
+(mobile works too), just put it somewhere in your __html__ file:
+````html
+<script src="node_modules/stupid-reload/index.js"></script>    
+````
+Then reload your page and dblclick somewhere. The url should change
+from ```example.com/index.html``` to ```example.com/index.html#live=true```
+and reloading each 4 seconds should start.
+
+## ToDo:
+* Upload to npm
+* Read config not only from data-config JSON,
+but also from the attributes like this:
+````html
+<script 
+    data-enable-reload="true"
+    type="text/javascript" 
+    src="node_modules/stupid-reload/index.js">
+</script>    
+````
+* Make node cli (should write script tag to itself into given file)
+* Make index.html for github pages with this README.md content and 
+a basic example.
---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * @name Stupid Reload
+ * @author a13ks3y.ass@gmail.com
+ * @licence WTFPL
+ *
+ * Basically, if you don't need to change reload state (switch off and then turn it on back) you can use just one line:
+ * <code>
+ *   setTimeout(location.reload.bind(location, [true], 0xfff));
+ * </code>
+ */
+(function () {
+    const getConfigFromScriptTagAttribute =  () => {
+        try {
+            // @todo use different techniques to get the currentScript and support old browsers @see https://stackoverflow.com/questions/403967/how-may-i-reference-the-script-tag-that-loaded-the-currently-executing-script
+            return JSON.parse(document.currentScript.getAttribute("data-config"));
+        } catch (e) {
+            console.warn("Script attribute configuration (data-config) is not a valid JSON!", e);
+            return {};
+        }
+    };
+    window.stupidReloadConfig = Object.assign(window.stupidReloadConfig || {}, {
+        hashParameterCheckString: "live=true",
+        reloadTimeout: 0xfff,
+    }, getConfigFromScriptTagAttribute());
+
+    const checkHashAndReload =
+        () => location.hash.search(stupidReloadConfig.hashParameterCheckString) !== -1 &&
+            (
+                checkHashAndReload.reloadTimerHandle =
+                setTimeout(location.reload.bind(location, [true]), stupidReloadConfig.reloadTimeout)
+            );
+    const toggleReloadState = () =>
+        (location.hash.search(stupidReloadConfig.hashParameterCheckString) !== -1 ?
+            (location.href = location.href.replace(stupidReloadConfig.hashParameterCheckString, "")) &&
+                clearTimeout(checkHashAndReload.reloadTimerHandle) :
+            (location.hash += stupidReloadConfig.hashParameterCheckString) && location.reload(true))
+        && checkHashAndReload();
+
+    // @todo: different events should be configurable
+    let _bodyDblClickLastTime = 0;
+    document.body.addEventListener("click",
+        () => _bodyDblClickLastTime === 0 ?
+            _bodyDblClickLastTime = Date.now() :
+            Date.now() - _bodyDblClickLastTime < 800 ?
+                toggleReloadState() && (_bodyDblClickLastTime = 0) :
+                _bodyDblClickLastTime = Date.now()
+    );
+    checkHashAndReload();
+}());
